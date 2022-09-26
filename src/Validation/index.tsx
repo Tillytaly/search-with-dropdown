be8bd404f90e514284a3e365c4b25555f022a367
Tillytaly <agataziemniak1@gmail.com>
2022-09-26 14:56:12 +0200

    Changed error messages

--- src/Validation/index.tsx
+++ src/Validation/index.tsx
@@ -1,6 +1,12 @@
 import * as yup from "yup";
 export const formikSchema = yup.object().shape({
-  name: yup.string().required().min(1),
-  regularPrice: yup.number().required().positive(),
-  salePrice: yup.number().required().positive(),
+  name: yup.string().required("Required field").min(1),
+  regularPrice: yup
+    .number()
+    .required("Required field")
+    .positive("Positive number required"),
+  salePrice: yup
+    .number()
+    .required("Required field")
+    .positive("Positive number required"),
 });
